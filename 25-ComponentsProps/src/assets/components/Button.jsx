193d954a860470370fbdd7d8ddb50c5448bb681e
shirinovashukriyye <shirinovashukriyye@gmail.com>
2025-04-22 12:47:43 +0400

    homework

---
+++ 25-ComponentsProps/src/assets/components/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Header.css";
+
+export default function Button() {
+  return (
+    <Button className='btn'>Found out fmore</Button>
+  )
+}
---
+++ 25-ComponentsProps/src/assets/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div >
+        <h1 >Stylish Portfolio</h1>
+        <h3>A Free Bootstrap Theme by Start Bootstrap</h3>
+    </div>
+  )
+}
